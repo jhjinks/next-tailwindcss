e81cb7cf00a91bc873c7143ba46d5e1873799eb9
Jinux-Web <jhjinks@jinuxweb.com>
2020-08-19 19:39:47 -0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = () => (
+  <div className="h-screen flex justify-center items-center bg-gray-200">
+    <h1 className="text-5xl font-bold text-blue-700">Next.js + Tailwind CSS</h1>
+  </div>
+);
+
+export default Index;
